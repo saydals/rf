4229f205b91a4abb0df57ffaf8dbd56b272a6326
saydals <73ajfEnddl@gmain.com>
2026-07-11 20:32:28 +0900

    Adjustments 탭 비동기 로드

--- src/js/tabs/adjustments.js
+++ src/js/tabs/adjustments.js
@@ -122,7 +122,11 @@ function getFunctions() {
 tab.initialize = function (callback) {
     const self = this;
 
-    load_data(load_html);
+    load_html();  // HTML immediately
+    // MSP data loads in background
+    load_data(function () {
+        data_to_form();
+    });
 
     function load_html() {
         $('#content').load("/src/tabs/adjustments.html", process_html);
